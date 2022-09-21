b5866d2245fad3e06718c420a8c0acefbfdcf963
Osvaldo Caetano <caetano.osvaldo@gmail.com>
2022-09-21 16:53:56 -0300

    Angular Initial

---
+++ src/app/nova_transferecia/nova_transferencia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "nova-transferencia",
+  templateUrl: "./nova_transferencia.component.html",
+  styleUrls: ['./nova_transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+}
